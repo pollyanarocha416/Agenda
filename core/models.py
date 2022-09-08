296c41c2a7fe916170c278bb570077283dc4cf4e
Pollyana <pollyanarocha416@gmail.com>
2022-09-07 21:24:38 -0300

    Criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from xml.parsers.expat import model
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+import datetime
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
@@ -19,3 +19,6 @@ class Evento(models.Model):
     
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
