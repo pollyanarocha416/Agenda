1f34f40a7cb878efb5b1f2a5bdce364aa67b432a
Pollyana <pollyanarocha416@gmail.com>
2022-09-12 18:54:17 -0300

    Incluido template para incercao de eventos da agenda. Incluido o gitiginore para iginorar compilados

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 # Create your views here.
 # def index(request):
 #    return redirect('/agenda/')
@@ -32,3 +33,21 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario) 
+    return redirect('/')                      
